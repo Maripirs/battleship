f7028ff95582d21755f7aae11d1ff5b80e97b9d2
Maripirs <mariapazmaluenda@gmail.com>
2023-12-07 13:46:27 -0800

    added helper funtions

---
+++ helpers.cpp
@@ -0,0 +1,8 @@
+#include "helpers.h"
+
+void createBoard(Player& pl) {
+    cout << "Create Board input is not implemented" << endl;
+}
+bool takeTurn(Player& pl) {
+    cout << "Create Player is not implemented" << endl;
+}
---
+++ helpers.h
@@ -0,0 +1,9 @@
+#include "player.h"
+void createBoard(Player&);
+
+bool takeTurn(Player&);
+
+template <typename T>
+T getValidInput(T min, T max) {
+    cout << "Get Valid input is not implemented" << endl;
+}
