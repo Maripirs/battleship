2d982925c12ae622482c0d645b52f1cf61fd3b01
Maripirs <mariapazmaluenda@gmail.com>
2023-12-07 11:46:12 -0800

    updated board

---
+++ board.cpp
@@ -0,0 +1,33 @@
+#include "board.h"
+
+Board::Board() {
+    rows = 10;
+    cols = 10;
+    isHuman = false;
+}
+Board::Board(Arr<Arr<Coor>>& b, int r, int c, bool h) {
+    board = b;
+    rows = r;
+    cols = c;
+    isHuman = h;
+}
+Board::Board(const Board& rhs) {
+    board = rhs.board;
+    rows = rhs.rows;
+    cols = rhs.cols;
+    isHuman = rhs.isHuman;
+}
+
+void Board::setBoard(Arr<Arr<Coor>>& b) { board = b; }
+void Board::setRows(int r) { rows = r; }
+void Board::setCols(int c) { cols = c; }
+
+void Board::setIsHuman(bool h) { isHuman = h; }
+
+Arr<Arr<Coor>>& Board::getBoard() { return board; }
+int Board::getRows() { return rows; }
+int Board::getCols() { return cols; }
+
+bool Board::getIsHuman() { return isHuman; }
+
+void Board::displayBoard() { cout << "Display Board not implemented" << endl; }
--- board.h
+++ board.h
@@ -1,5 +1,31 @@
 #ifndef BOARD_H
 #define BOARD_H
-#include "player.h"
-class User : public Player {};
+#include "Arr.h"  //Don't think we need it
+#include "Coor.h"
+class Board {
+    Arr<Arr<Coor>> board;
+    int rows;
+    int cols;
+
+    bool isHuman;
+
+   public:
+    Board();
+    Board(Arr<Arr<Coor>>&, int, int, bool);
+    Board(const Board&);
+
+    void setBoard(Arr<Arr<Coor>>&);
+    void setRows(int);
+    void setCols(int);
+
+    void setIsHuman(bool);
+
+    Arr<Arr<Coor>>& getBoard();
+    int getRows();
+    int getCols();
+
+    bool getIsHuman();
+
+    void displayBoard();
+};
 #endif
