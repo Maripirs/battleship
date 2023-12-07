fd607569d57f2a3c8ad24f233f576bc60fc08d61
Maripirs <mariapazmaluenda@gmail.com>
2023-12-07 11:26:55 -0800

    updated ship

---
+++ ship.cpp
@@ -0,0 +1,39 @@
+#include "ship.h"
+Ship::Ship() {
+    size = -1;
+    name = "NONE";
+    hits = 0;
+}
+Ship::Ship(int s, string n, int h, Arr<Coor> c) {
+    size = s;
+    name = n;
+    hits = h;
+    coors = c;
+}
+Ship::Ship(const Ship& rhs) {
+    size = rhs.size;
+    name = rhs.name;
+    hits = rhs.hits;
+    coors = rhs.coors;
+}
+
+int Ship::getSize() { return size; }
+string Ship::getName() { return name; }
+int Ship::getHits() { return hits; }
+Arr<Coor>& Ship::getCoors() { return coors; }
+
+void Ship::setSize(int s) { size = s; }
+void Ship::setName(string n) { name = n; }
+void Ship::setHits(int h) { hits = h; }
+void Ship::setCoors(Arr<Coor>& c) { coors = c; }
+
+bool Ship::isHit(Coor) { cout << "Is Hit not implemented" << endl; }
+bool Ship::isDead() { cout << "Is Dead not implemented" << endl; }
+
+Ship& Ship::operator=(const Ship& rhs) {
+    size = rhs.size;
+    name = rhs.name;
+    hits = rhs.hits;
+    coors = rhs.coors;
+    return *this;
+}
--- ship.h
+++ ship.h
@@ -1,4 +1,31 @@
 #ifndef SHIP_H
 #define SHIP_H
-class Ship{};
+#include "arr.h"
+#include "coor.h"  //Don't think we need this one.
+class Ship {
+    int size;
+    string name;
+    int hits;
+    Arr<Coor> coors;
+
+   public:
+    Ship();
+    Ship(int, string, int, Arr<Coor>);
+    Ship(const Ship&);
+
+    int getSize();
+    string getName();
+    int getHits();
+    Arr<Coor>& getCoors();
+
+    void setSize(int);
+    void setName(string);
+    void setHits(int);
+    void setCoors(Arr<Coor>&);
+
+    bool isHit(Coor);
+    bool isDead();
+
+    Ship& operator=(const Ship&);
+};
 #endif
