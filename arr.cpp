2da24d7363d0a54bfb13b5e52ceea9c939733b63
Maripirs <mariapazmaluenda@gmail.com>
2023-12-07 09:50:46 -0800

    updated dynamic array class

---
+++ arr.cpp
@@ -0,0 +1,98 @@
+
+template <class T>
+Arr<T>::Arr() {
+    arr = new T[1];
+    num = 0;
+    max = 1;
+}
+template <class T>
+Arr<T>::Arr(T* a, int c, int m) {
+    num = c;
+    max = m;
+    arr = new T[max];
+    for (int i = 0; i < num; i++) {
+        arr[i] = a[i];
+    }
+}
+template <class T>
+Arr<T>::Arr(const Arr& rhs) {
+    num = rhs.num;
+    max = rhs.max;
+    arr = new T[max];
+    for (int i = 0; i < num; i++) {
+        arr[i] = rhs.arr[i];
+    }
+}
+
+template <class T>
+T* Arr<T>::getArr() {
+    return arr;
+}
+template <class T>
+T& Arr<T>::getAtIndex(int idx) {
+    return arr[idx];
+}
+template <class T>
+int Arr<T>::getNum() {
+    return num;
+}
+template <class T>
+int Arr<T>::getMax() {
+    return max;
+}
+
+template <class T>
+void Arr<T>::addToEnd(T item) {
+    if (num == max) {
+        max = max + 1;
+        T* temp = new T[max];
+        for (int i = 0; i < num; i++) {
+            temp[i] = arr[i];
+        }
+        delete[] arr;
+        arr = temp;
+    }
+    arr[num] = item;
+    num++;
+}
+template <class T>
+void Arr<T>::replaceAtIndex(int idx, T item) {
+    arr[idx] = item;
+}
+template <class T>
+void Arr<T>::removeAtIndex(int idx) {
+    T* temp = new T[max];
+    for (int i = 0; i < idx; i++) {
+
+        temp[i] = arr[i];
+    }
+    for (int i = idx; i < num ; i++) {
+
+        temp[i] = arr[i+1];
+    }
+    delete[] arr;
+    arr = temp;
+    num--;
+}
+template <class T>
+void Arr<T>::clearArr() {
+    delete[] arr;
+    arr = new T[1];
+    num = 0;
+    max = 1;
+}
+template <class T>
+Arr<T>::~Arr() {
+    delete[] arr;
+}
+
+template <class T>
+Arr<T>& Arr<T>::operator=(const Arr& rhs) {
+    num = rhs.num;
+    max = rhs.max;
+    arr = new T[max];
+    for (int i = 0; i < num; i++) {
+        arr[i] = rhs.arr[i];
+    }
+    return *this;
+}
--- arr.h
+++ arr.h
@@ -1,5 +1,28 @@
 #ifndef ARR_H
 #define ARR_H
 
-class Arr {};
+template <class T>
+class Arr {
+    T* arr;
+    int num;
+    int max;
+
+   public:
+    Arr();
+    Arr(T*, int, int);
+    Arr(const Arr&);
+
+    T* getArr();
+    T& getAtIndex(int);
+    int getNum();
+    int getMax();
+
+    void addToEnd(T);
+    void replaceAtIndex(int, T);
+    void removeAtIndex(int);
+    void clearArr();
+    ~Arr();
+    Arr& operator=(const Arr&);
+};
+#include "arr.cpp"
 #endif
